40cc36697cd4e1cede75083ec5c2865c4c6ab909
Vasi <157385207+Vasi420@users.noreply.github.com>
2024-06-07 08:57:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "10046731"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "09a25ea3e558df7e5d969c144b0452d9"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "1808943146")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = getenv("OWNER_ID", "5981008313")
+SUPPORT_GRP = "the_comedy_bar"
+UPDATE_CHNL = "AS_Bots_Updates"
+OWNER_USERNAME = "Oxygen_420"
 
